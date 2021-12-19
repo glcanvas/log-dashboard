362154914482e9b58ef72d1b6641afbda8bf8327
Vladimir Kalnitsky <klntsky@gmail.com>
2021-12-19 23:32:25 +0300

    Fix generator in docker. Add table definitions.

--- db/init.sql
+++ db/init.sql
@@ -2,6 +2,8 @@ SELECT cluster, shard_num, host_name, host_address, port, is_local FROM system.c
 
 CREATE DATABASE db ON CLUSTER webshop;
 
+-- request_trace
+
 CREATE TABLE IF NOT EXISTS db.request_trace ON CLUSTER webshop
 (
     trace_id Int64,
@@ -25,45 +27,167 @@ SETTINGS
   kafka_format = 'JSONEachRow',
   kafka_max_block_size = 1000;
 
-
 CREATE MATERIALIZED VIEW
   db.distr_request_trace_kafka_consumer
 TO db.distr_request_trace
 AS SELECT *
 FROM db.distr_request_trace_kafka;
 
--- { trace_id: 123, time: "2018-07-10T12:30:18.749Z", log: "bruh" }
+-- user_trace
 
--- TMP: actions. TODO: remove, or it will eat messages instead of spark!
-
-
-CREATE TABLE IF NOT EXISTS db.gen_logs ON CLUSTER webshop
+CREATE TABLE IF NOT EXISTS db.user_trace ON CLUSTER webshop
 (
-        action String
+    user_id Int64,
+    time DateTime,
+    log String
 )
 ENGINE = MergeTree()
-PARTITION BY action
-ORDER BY (action);
+PARTITION BY toYear(time)
+ORDER BY (time);
 
-CREATE TABLE db.distr_gen_logs
-ON CLUSTER webshop AS db.gen_logs
-  ENGINE = Distributed(webshop, db, gen_logs, xxHash64(action));
+CREATE TABLE db.distr_user_trace
+ON CLUSTER webshop AS db.user_trace
+  ENGINE = Distributed(webshop, db, user_trace, xxHash64(user_id));
 
-CREATE TABLE db.distr_gen_logs_kafka AS db.distr_gen_logs
+CREATE TABLE db.distr_user_trace_kafka AS db.distr_user_trace
 ENGINE = Kafka()
 SETTINGS
   kafka_broker_list = 'kafka:9092,kafka:9094',
-  kafka_topic_list = 'gen-logs',
+  kafka_topic_list = 'user_trace',
   kafka_group_name = 'main',
   kafka_format = 'JSONEachRow',
   kafka_max_block_size = 1000;
 
 CREATE MATERIALIZED VIEW
-  db.distr_gen_logs_kafka_consumer
-TO db.distr_gen_logs
+  db.distr_user_trace_kafka_consumer
+TO db.distr_user_trace
 AS SELECT *
-FROM db.distr_gen_logs_kafka;
+FROM db.distr_user_trace_kafka;
 
-SET stream_like_engine_allow_direct_select = true;
+-- online_user
 
-SELECT * FROM db.distr_gen_logs;
+CREATE TABLE IF NOT EXISTS db.online_user ON CLUSTER webshop
+(
+    count Int64,
+    time DateTime
+)
+ENGINE = MergeTree()
+PARTITION BY toYear(time)
+ORDER BY (time);
+
+CREATE TABLE db.distr_online_user
+ON CLUSTER webshop AS db.online_user
+  ENGINE = Distributed(webshop, db, online_user, xxHash64(count));
+
+CREATE TABLE db.distr_online_user_kafka AS db.distr_online_user
+ENGINE = Kafka()
+SETTINGS
+  kafka_broker_list = 'kafka:9092,kafka:9094',
+  kafka_topic_list = 'online_user',
+  kafka_group_name = 'main',
+  kafka_format = 'JSONEachRow',
+  kafka_max_block_size = 1000;
+
+CREATE MATERIALIZED VIEW
+  db.distr_online_user_kafka_consumer
+TO db.distr_online_user
+AS SELECT *
+FROM db.distr_online_user_kafka;
+
+-- failed_login
+
+CREATE TABLE IF NOT EXISTS db.failed_login ON CLUSTER webshop
+(
+    user_id Int64,
+    time DateTime
+)
+ENGINE = MergeTree()
+PARTITION BY toYear(time)
+ORDER BY (time);
+
+CREATE TABLE db.distr_failed_login
+ON CLUSTER webshop AS db.failed_login
+  ENGINE = Distributed(webshop, db, failed_login, xxHash64(user_id));
+
+CREATE TABLE db.distr_failed_login_kafka AS db.distr_failed_login
+ENGINE = Kafka()
+SETTINGS
+  kafka_broker_list = 'kafka:9092,kafka:9094',
+  kafka_topic_list = 'failed_login',
+  kafka_group_name = 'main',
+  kafka_format = 'JSONEachRow',
+  kafka_max_block_size = 1000;
+
+CREATE MATERIALIZED VIEW
+  db.distr_failed_login_kafka_consumer
+TO db.distr_failed_login
+AS SELECT *
+FROM db.distr_failed_login_kafka;
+
+-- users_spend_time_online
+
+CREATE TABLE IF NOT EXISTS db.users_spend_time_online ON CLUSTER webshop
+(
+    user_id Int64,
+    time DateTime,
+    spent Int64
+)
+ENGINE = MergeTree()
+PARTITION BY toYear(time)
+ORDER BY (time);
+
+CREATE TABLE db.distr_users_spend_time_online
+ON CLUSTER webshop AS db.users_spend_time_online
+  ENGINE = Distributed(webshop, db, users_spend_time_online, xxHash64(user_id));
+
+CREATE TABLE db.distr_users_spend_time_online_kafka AS db.distr_users_spend_time_online
+ENGINE = Kafka()
+SETTINGS
+  kafka_broker_list = 'kafka:9092,kafka:9094',
+  kafka_topic_list = 'users_spend_time_online',
+  kafka_group_name = 'main',
+  kafka_format = 'JSONEachRow',
+  kafka_max_block_size = 1000;
+
+CREATE MATERIALIZED VIEW
+  db.distr_users_spend_time_online_kafka_consumer
+TO db.distr_users_spend_time_online
+AS SELECT *
+FROM db.distr_users_spend_time_online_kafka;
+
+
+-- this table moves logs directly. we don't use it. we inserted spark between logs and our CH tables.
+
+-- CREATE TABLE IF NOT EXISTS db.gen_logs ON CLUSTER webshop
+-- (
+--         action String
+-- )
+-- ENGINE = MergeTree()
+-- PARTITION BY action
+-- ORDER BY (action);
+
+-- CREATE TABLE db.distr_gen_logs
+-- ON CLUSTER webshop AS db.gen_logs
+--   ENGINE = Distributed(webshop, db, gen_logs, xxHash64(action));
+
+-- CREATE TABLE db.distr_gen_logs_kafka AS db.distr_gen_logs
+-- ENGINE = Kafka()
+-- SETTINGS
+--   kafka_broker_list = 'kafka:9092,kafka:9094',
+--   kafka_topic_list = 'gen-logs',
+--   kafka_group_name = 'main',
+--   kafka_format = 'JSONEachRow',
+--   kafka_max_block_size = 1000;
+
+-- CREATE MATERIALIZED VIEW
+--   db.distr_gen_logs_kafka_consumer
+-- TO db.distr_gen_logs
+-- AS SELECT *
+-- FROM db.distr_gen_logs_kafka;
+
+
+-- SELECT * FROM db.distr_gen_logs;
+
+-- Use this to test kafka:
+-- it will allow running SELECT on kafka streams as tables
+-- SET stream_like_engine_allow_direct_select = true;
